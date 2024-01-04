6268a941f44fdbbdb44426a5a7f92f25caa3eb7a
anton-tchekov <anton@tchekov.net>
2024-01-04 14:33:38 +0000

    Formatting

--- modules/modfunc.h
+++ modules/modfunc.h
@@ -1,12 +1,8 @@
-//
-// Created by tim on 11/26/23.
-//
-
-#ifndef OSDEV_X64_MODFUNC_H
-#define OSDEV_X64_MODFUNC_H
+#ifndef __MODFUNC_H__
+#define __MODFUNC_H__
 
 typedef void (*printf_fn)(const char *s, ...);
 
 extern printf_fn printf;
 
-#endif //OSDEV_X64_MODFUNC_H
+#endif
--- modules/module.c
+++ modules/module.c
@@ -4,10 +4,10 @@
 
 printf_fn printf = NULL;
 
-void load_functions(void* data)
+void load_functions(void *data)
 {
-    ModuleInit *mi = (ModuleInit *)data;
-    uint64_t *fns = mi->Functions;
+	ModuleInit *mi = (ModuleInit *)data;
+	uint64_t *fns = mi->Functions;
 
-    printf = (printf_fn)fns[KERNEL_FN_PRINTF];
+	printf = (printf_fn)fns[KERNEL_FN_PRINTF];
 }
